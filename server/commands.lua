4f8100f8e7eeb0f0bb7f1ce78b712360a9af85e0
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-06-13 08:18:22 -0300

    fix: Remover arquivos nao utilizados

--- server/commands.lua
+++
@@ -1,7 +0,0 @@
-
-lib.addCommand('managefarms', {
-    help = 'Este comando gerencia os farms do servidor (Somente Admin).',
-    restricted = 'group.admin'
-}, function(source, args, raw)
-    TriggerClientEvent("mri_qfarm:Client:ManageFarms", source)
-end)
